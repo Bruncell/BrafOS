68803bf07991aeabc3ce658217b755c621fd3427
Bruncell <bruncellq@gmail.com>
2025-07-11 11:46:22 +0000

    Update graphic.c

--- src/graphic.c
+++ src/graphic.c
@@ -19,26 +19,20 @@ x *= 8;
 y *=10;
 
 int temp_x = x;
-   					for(int i = 0; i < 10; i++)
-  				    {
-  				    uint8_t bits = font[LetterID][i];
-  				   	for(int j = 0; j < 8; j++)
-  				   	{
+	
+ for(int i = 0; i < 10; i++)
+ {
+ 	  uint8_t bits = font[LetterID][i];
+	  for(int j = 0; j < 8; j++)
+ 	  {
+ 	  uint8_t bit = (bits >> (7 - j)) & 1;
+	  uint32_t color = bit ? FrontColor : BackColor;
+ 	  pix(x, y, color);
+	  x++;	
+	  }
+	  x = temp_x;
+	  y++;
 
-					
-						uint8_t bit = (bits >> (7 - j)) & 1;
-						uint32_t color = bit ? FrontColor : BackColor;
-						
-  				 	          pix(x, y, color);
-  				 	 
-  				 	    
-  				 	  x++;	
-  				   	}
-  				   	  x = temp_x;
-  				   	  y++;
-
-
-
-  				 }
+ }
 }
 
